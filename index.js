a7883acfc26ebd12c20db728abd16ccd63034583
Guilherme Arndt de Oliveira <guilherme.arndt@gmail.com>
2022-03-29 20:54:25 -0300

    fix index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const app = express();
 
-app.get("/", function(req, res){
+app.get("/hello", function(req, res){
     res.send("Hello World");
 });
 
 app.listen(process.env.PORT || 8089, function(){
-    console.log("Servidor Rodando");
+    console.log("Servidor Rodando", process.env.PORT);
 });
