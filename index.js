0235200ab6cda6606784631e8e746f3edb95a4f4
Guilherme Arndt de Oliveira <guilherme.arndt@gmail.com>
2022-03-29 20:25:28 -0300

    replace main.js

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require("express");
 const app = express();
 
 app.get("/", function(req, res){
-    res.send("Hello World 5");
+    res.send("Hello World");
 });
 
-app.listen(21116, function(){
+app.listen(80, function(){
     console.log("Servidor Rodando");
 });
